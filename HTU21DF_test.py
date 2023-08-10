9980ac4c796b3b50b19597a41c40924134ac986b
Mate Timko <timko.mate@gmail.com>
2023-08-10 20:29:49 +0100

    Add HTU21DF test
    Script that checks if the HTU21DF sensor is connected properly

---
+++ HTU21DF_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import datetime
+import sys
+import time
+import board
+import busio
+from adafruit_htu21d import HTU21D
+
+dt = 3
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = HTU21D(i2c)
+
+while True:
+    temperature = sensor.temperature
+    humidity = sensor.relative_humidity
+    print(f"temperature: {temperature:.2f}, humidity: {humidity:.2f}")
+    time.sleep(dt)
