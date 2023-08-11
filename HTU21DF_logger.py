e6b41d1cc7f121aeb0e8f6e1f96f35a0d9b7742c
Mate Timko <timko.mate@gmail.com>
2023-08-11 19:07:07 +0200

    Logging mechanism is introduced

--- HTU21DF_logger.py
+++ HTU21DF_logger.py
@@ -5,20 +5,28 @@ import busio
 import mariadb
 from adafruit_htu21d import HTU21D
 import json
-
+import logging
 
 class SensorDataLogger:
     def __init__(self, config_path, secrets_path):
         self.config = self.load_json(config_path)
         self.secrets = self.load_json(secrets_path)
+        self.init_logging()
         self.init_database_connection()
         self.init_sensor()
 
     @staticmethod
     def load_json(file_path):
-        with open(file_path, "r") as json_file:
+        with open(file_path, 'r') as json_file:
             data = json.load(json_file)
         return data
+
+    def init_logging(self):
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        self.logger = logging.getLogger(__name__)
 
     def init_database_connection(self):
         try:
@@ -28,12 +36,12 @@ class SensorDataLogger:
                 host=self.config["host"],
                 port=self.config["port"],
                 database=self.config["database"],
-                autocommit=False,
+                autocommit=False
             )
             self.cur = self.conn.cursor()
-            print("Database connection is ready")
+            self.logger.info("Database connection is ready")
         except mariadb.Error as e:
-            print(f"An error occurred while connecting to the database: {e}")
+            self.logger.error(f"An error occurred while connecting to the database: {e}")
             sys.exit(1)
 
     def init_sensor(self):
@@ -41,7 +49,7 @@ class SensorDataLogger:
             i2c = busio.I2C(board.SCL, board.SDA)
             self.sensor = HTU21D(i2c)
         except Exception as e:
-            print(f"An error occurred while initializing the sensor: {e}")
+            self.logger.error(f"An error occurred while initializing the sensor: {e}")
             sys.exit(1)
 
     def log_sensor_data(self):
@@ -51,25 +59,25 @@ class SensorDataLogger:
             humidity = self.sensor.relative_humidity
             now = datetime.datetime.utcnow()
             query = f"INSERT INTO {self.config['table_name']} (date, temperature, humidity) VALUES ('{now}', '{temperature}', '{humidity}')"
-            print(query)
+            self.logger.info(query)
             try:
                 self.cur.execute(query)
                 self.conn.commit()
             except mariadb.Error as e:
-                print(f"An error occurred while inserting data into the database: {e}")
+                self.logger.error(f"An error occurred while inserting data into the database: {e}")
                 self.conn.rollback()
             time.sleep(self.config["dt"])
 
     def run(self):
         try:
-            print("Starting sensor data logging...")
+            self.logger.info("Starting sensor data logging...")
             self.log_sensor_data()
         finally:
             self.cur.close()
             self.conn.close()
-            print("Database connection closed")
-
+            self.logger.info("Database connection closed")
 
 if __name__ == "__main__":
     logger = SensorDataLogger("./config.json", "./secrets.json")
     logger.run()
+
